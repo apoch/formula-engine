2ff2bee7a70e0e28c7dc1c11edfae91cb95c14eb
don.apoch@gmail.com <don.apoch@gmail.com>
2015-09-13 17:30:02 -0700

    Document Token Pool interface and implementation

--- FormulaEngine/TokenPool.cpp
+++ FormulaEngine/TokenPool.cpp
@@ -1,8 +1,27 @@
+//
+// FormulaEngine Project
+// By Mike Lewis - 2015
+//
+// Mapper for condensing strings to unique unsigned integer IDs
+//
+// This module implements a simple and minimal version of the token
+// pool concept, based on the requirements detailed in TokenPool.h.
+//
+// Note that we choose to use monotonic increasing IDs based on
+// indices into a vector; this is not as efficient as it could be,
+// but it's effective and easy to think about. Most of the usage
+// of this object is meant to occur at load time and/or duing a
+// heavy debugging session, so performance is not yet a concern.
+//
+
 #include "Pch.h"
 
 #include "TokenPool.h"
 
 
+//
+// Add a string into the pool and get its token back
+//
 unsigned TokenPool::AddToken(const std::string & token) {
 	for(unsigned i = 0; i < m_pool.size(); ++i) {
 		if(m_pool[i] == token)
@@ -14,7 +33,10 @@ unsigned TokenPool::AddToken(const std::string & token) {
 }
 
 
-const std::string & TokenPool::GetToken(unsigned token) const {
+//
+// Given a token, look up the original string for it
+//
+const std::string & TokenPool::GetStringFromToken(unsigned token) const {
 	assert(token != 0);
 	
 	unsigned index = token - 1;
--- FormulaEngine/TokenPool.h
+++ FormulaEngine/TokenPool.h
@@ -1,10 +1,24 @@
+//
+// FormulaEngine Project
+// By Mike Lewis - 2015
+//
+// Mapper for condensing strings to unique unsigned integer IDs
+//
+// Given an input string, the token pool is designed to return a
+// unique but stable ID - that is, if the same string is provided
+// to the token pool later on, the same ID should be retured.
+//
+// Moreover, the token pool must support going from a unique ID
+// back to the original string.
+//
+
 #pragma once
 
 
 class TokenPool {
 public:
 	unsigned AddToken(const std::string & str);
-	const std::string & GetToken(unsigned token) const;
+	const std::string & GetStringFromToken(unsigned token) const;
 
 private:
 	std::vector<std::string> m_pool;
